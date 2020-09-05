d28356a319286e48b62f1aa363e87563a29869a5
ningpop <craft1933@naver.com>
2020-09-05 16:19:11 +0900

    made apps

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'freinds.apps.FriendsConfig',
+    'columns.apps.ColumnsConfig',
+    'reports.apps.ReportsConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
