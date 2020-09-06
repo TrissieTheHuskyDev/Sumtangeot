590fafd7dd79ea571e4fabcfcdaf8d00636e295c
ningpop <craft1933@naver.com>
2020-09-06 16:14:00 +0900

    connect friends detail page

--- friends/urls.py
+++ friends/urls.py
@@ -3,8 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('a/', views.a, name='a'),
+    path('detail/<int:friend_id>', views.detail, name='detail'),
     path('sum/', views.sum, name='sum'),
     path('donation/', views.donation, name='donation'),
-    path('animal/', views.animal, name='animal'),
 ]
--- friends/views.py
+++ friends/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Friend
 from columns.models import Column
 
@@ -9,16 +9,12 @@ def index(request):
     column_list = Column.objects.all()
     return render(request, 'index.html', {'friend_list':friend_list, 'column_list':column_list})
 
-def a(request):
-    return render(request, 'a.html')
+def detail(request, friend_id):
+    friend = get_object_or_404(Friend, pk=friend_id)
+    return render(request, 'detail.html', {'friend':friend})
 
 def sum(request):
     return render(request, 'sum.html')
 
 def donation(request):
     return render(request, 'donation.html')
-
-def animal(request):
-    friend_list = Friend.objects.all()
-    column_list = Column.objects.all()
-    return render(request, 'animal.html', {'friend_list':friend_list, 'column_list':column_list})
