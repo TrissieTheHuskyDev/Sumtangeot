311afd2badcfd34204c032e88bcf4d178bf5f128
HwangSB <hsb990105@gmail.com>
2020-09-06 18:46:13 +0900

    report manage page - 제보 관리 페이지의 마커 작동 방식 수정

---
+++ reports/static/js/report_manage.js
@@ -0,0 +1,24 @@
+let container = document.getElementById('map');
+
+let options = {
+    center: new kakao.maps.LatLng(36.76945236756954, 126.93164179306778),
+    level: 5
+};
+
+let map = new kakao.maps.Map(container, options);
+
+function createMarker(latitude, longitude, content) {
+    let marker = new kakao.maps.Marker({  
+        map: map, 
+        position: new kakao.maps.LatLng(latitude, longitude)
+    });
+
+    kakao.maps.event.addListener(marker, 'click', function() {
+        infowindow = new kakao.maps.InfoWindow({
+            content : content,
+            removable : true
+        });
+        
+        infowindow.open(map, marker);
+    });
+}
